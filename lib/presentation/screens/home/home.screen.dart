8db6d7c103199864cf686ed2079ae8de51e4395a
Clement Guyon <clement.guyon@orkester.fr>
2025-08-05 09:23:23 +0200

    feat: v2

---
+++ lib/presentation/screens/home/home.screen.dart
@@ -0,0 +1,30 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// Home Screem
+@RoutePage()
+class HomeScreen extends ConsumerStatefulWidget {
+  /// Constructor
+
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Home'),
+          ],
+        ),
+      ),
+    );
+  }
+}
